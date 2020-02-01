12cf25336899c63cebe7c16b9d884c5c4745f099
Antoinette <create@aplusmedia.info>
2020-01-31 22:38:59 -0500

    i think I'm done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react';
 
-it('renders without crashing', () => {
+it('App renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('playercard is rendering data', () => {
+  const {getByTestId} = render(<PlayerCard />);
+
+  getByTestId(playerName/i);
+  getByTestId(countryName/i);
+  getByTestId(searchNumber/i);
+ 
+})
